b0be3e619e5e0144064fe8c9847904767bc5ef71
nawaz915 <maliknawaz1997@gmail.com>
2021-05-17 03:21:55 +0500

    routing basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import  HomePage from './pages/homepage/homepage.component';
+import  {Route} from 'react-router-dom';
 
 import './App.css';
+
+const HatsPage = () => (
+<div>
+  <h1>HATS PAGE</h1>
+</div>
+
+);
 
 function App() {
   return (
