fba6178d51bdc7240349b603645767b6ed964eb4
nawaz915 <maliknawaz1997@gmail.com>
2021-05-17 12:33:37 +0500

    collections added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import  HomePage from './pages/homepage/homepage.component';
-import  {Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import  {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 
-const HatsPage = () => (
-<div>
-  <h1>HATS PAGE</h1>
-</div>
-
-);
-
 function App() {
   return (
-    <HomePage />
+    <div>
+    <Switch>
+    <Route exact path= '/'  component={HomePage}  />
+    <Route path= '/shop'  component={ShopPage}  />
+    </Switch>
+    </div>
   );
 }
 
