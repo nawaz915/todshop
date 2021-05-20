776e4e24fb2c51f70f684e4e550229c123b0e070
nawaz915 <maliknawaz1997@gmail.com>
2021-05-21 03:01:09 +0500

    efa

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,27 @@
 import React from 'react'; 
+import {connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../../components/cart-item/cart-item.component';
+import {selectCartItems } from '../../redux/cart/cart.selectors';
 
-import './cart-component.styles.scss';
+import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
 
     <div className="cart-dropdown">
         <div className="cart-items">
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        {
+            cartItems.map(cartItem => (<CartItem key={cartItem.id} item={cartItem} />))
+        }    
         
         </div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
     
     </div>
-)
+);
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import  { withRouter } from 'react-router-dom';
 import SHOP_DATA from './shop.data.js';
 import './shop.styles.scss';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
 constructor (props) {
