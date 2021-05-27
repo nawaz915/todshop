78bb1c026bf03ad0aaa22e4f319e93a053ebcfde
nawaz915 <maliknawaz1997@gmail.com>
2021-05-28 03:44:52 +0500

    fazfedva

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import  { withRouter } from 'react-router-dom';
-import SHOP_DATA from './shop.data.js';
 import './shop.styles.scss';
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-class ShopPage extends React.Component {
-constructor (props) {
-    super(props);
-
-    this.state = {
-        collections : SHOP_DATA 
-    };
-}
-    render () {
-        const { collections } = this.state;
-        return (<div className="shop-page">
-        { collections.map( ({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps}/>
-         ))}
+const  ShopPage = ({collections}) => (
+    
+    <div className="shop-page">
+    
+    <CollectionOverview  />
+         
+    </div>
             
-            </div>);
+);
 
-    }
-}
+
+
 
 export default ShopPage;
